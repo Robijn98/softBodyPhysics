0382ba1946d281bbc15bb356667f985b7ef30321
Robin <167987187+Robijn98@users.noreply.github.com>
2025-05-26 17:46:15 +0100

    update shapes
    update shapes

--- shape_utils.py
+++ shape_utils.py
@@ -29,15 +29,18 @@ class SoftBodyObj():
 
 
 def create_formations():
-    #create_square( start_pos=Vector2(100, 100), size=30)
-    #create_triangle(start_pos=Vector2(200, 200), size=60)
+    create_square( start_pos=Vector2(100, 100), size=30)
+    create_triangle(start_pos=Vector2(200, 200), size=60)
+    create_square(start_pos=Vector2(300, 300), size=50)
     #create_wheel(start_pos=Vector2(300, 300), size=30, num_balls=10)
     #create_wheel(start_pos=Vector2(300, 300), size=60, num_balls=10)
     # create_wheel(start_pos=Vector2(300, 300), size=50, num_balls=10)
     # create_joint(objects[0].vertices[10], objects[1].vertices[10])
-    #create_platform(start_pos=Vector2(400, 400), size=50)
+    create_platform(start_pos=Vector2(400, 400), size=50)
+    create_platform(start_pos=Vector2(500, 500), size=50)
     
-    #create small squares
+    
+    # #create small squares
     # x, y = 10, 10
     # size = 20
     # spacing = 40  # distance between squares
